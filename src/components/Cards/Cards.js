09c252e5c1af1804a76b59565b9901198005d65e
Bruno Simone <brunosimone.mdq@gmail.com>
2022-10-04 22:18:17 -0300

    character cards added

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -9,10 +9,10 @@ if(results) {
   display = results.map(x=>{
     let { id, name, image, location, status } = x;
     return( 
-    <div key={id} className='col-4'>
+    <div key={id} className='col-4 mb-4 position-relative'>
       <div className={styles.cards}>
         <img src={image} alt='' className={`img-fluid`}/>
-        <div className='content'>
+        <div style={{ padding: "10px" }} className='content'>
           <div className='fs-4 fw-bold mb-4'>{name}</div>
           <div className=''>
             <div className='fs-6'>Last location</div>
@@ -20,9 +20,29 @@ if(results) {
           </div>
         </div> 
       </div>
-      <div className={`${styles.badge} position-absolute badge bg-danger`}>
+      {(()=>{ 
+        if(status==="Dead"){
+          return(
+            <div className={`${styles.badge} position-absolute badge bg-danger`}>
         {status}
       </div>
+          )
+        }
+        else if(status==="Alive"){
+          return(
+            <div className={`${styles.badge} position-absolute badge bg-success`}>
+        {status}
+      </div>
+          )
+        }
+        else{
+          return(
+          <div className={`${styles.badge} position-absolute badge bg-secondary`}>
+        {status}
+      </div>)
+        }
+      })()}
+      
     </div>
     );
   });
