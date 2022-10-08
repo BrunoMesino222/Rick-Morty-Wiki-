1219476acc2dca3792cf6cca3e33bce9a5c9efbd
betaniagonzalez <betaniagonzalez31@gmail.com>
2022-10-08 03:55:42 -0300

    search bar updated

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import Search from "./components/Search/Search";
 function App() {
 let [pageNumber, setPageNumber] = useState(1);
 let [fetchedData, updateFetchedData] = useState([]);
+let [search, setSearch] = useState("");
 
 let { info, results } = fetchedData; 
 
-let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
 useEffect(() => {
   (async function(){ 
@@ -28,7 +29,7 @@ useEffect(() => {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">Wiki</span>
       </h1>
-      <Search/>
+      <Search setPageNumber={setPageNumber} setSearch={setSearch}/>
         <div className="container">
           <div className="row">
             <div className="col-3">
